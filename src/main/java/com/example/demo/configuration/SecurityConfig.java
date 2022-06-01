1d6fe425ba263d8b33d44ac66745230f8d9d922c
KristinaMesyachik <mesyachik2001@gmail.com>
2022-06-01 12:08:29 +0300

    Add class exception and update config

---
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.demo.configuration;
+
+import com.example.demo.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/api/users/save/*","/api/users/registration/*", "index",  "/css/*", "/image/*", "/js/*")
+                .permitAll()
+//                .anyRequest().authenticated()//при переходе проверка регистрации
+                .and()
+                .formLogin()
+//                .loginPage("/api/user/login")
+                .permitAll()
+                .defaultSuccessUrl("/api/fields/", true)//Страница после входа
+        ;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+}
---
+++ src/main/java/com/example/demo/exception/PortalGlobalExceptionHandling.java
@@ -0,0 +1,26 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class PortalGlobalExceptionHandling {
+
+    @ExceptionHandler
+    public ResponseEntity<PortalIncorrectData> handleException(NoSuchPortalException exception) {
+        PortalIncorrectData portalIncorrectData= new PortalIncorrectData();
+        portalIncorrectData.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(portalIncorrectData, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<PortalIncorrectData> handleException(Exception exception) {
+        PortalIncorrectData employeeIncorrectData = new PortalIncorrectData();
+        employeeIncorrectData.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(employeeIncorrectData, HttpStatus.BAD_REQUEST);
+    }
+}
