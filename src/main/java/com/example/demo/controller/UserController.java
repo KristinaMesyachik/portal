7631a4d9e40005c0b22f14e2097ceaa515d608db
KristinaMesyachik <mesyachik2001@gmail.com>
2022-06-06 06:35:40 +0300

    Add mail

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.IMailService;
 import com.example.demo.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,16 +14,20 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/api/users")
+//@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
-    public String login(HttpSession session) {
-        return "login";
-    }
+    @Autowired
+    private IMailService mailService;
+//
+//    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+//    public String login() {
+////        User user = new User();
+//        return "login";
+//    }
 
     @GetMapping("/registration")
     public String addUserPage(Model model) {
@@ -31,10 +36,20 @@ public class UserController {
         return "registration";
     }
 
+    @RequestMapping(value = {"/registrationSave"}, method = RequestMethod.POST)
+    public String registrationSave(@ModelAttribute(name = "user") User user) {
+        userService.registrationSave(user);
+        mailService.sendSimpleEmail(user.getUsername(),
+                "About registration",
+                "You have successfully registered");
+        return "redirect:/api/fields/";
+    }
+
     @RequestMapping(value = {"/save"}, method = RequestMethod.POST)
     public String save(@ModelAttribute(name = "user") User user) {
-        userService.save(user);
-        return "success-submit";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        userService.save(user, auth.getName());
+        return "redirect:/api/fields/";
     }
 
     @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
@@ -42,15 +57,15 @@ public class UserController {
     public String afterRegistration(HttpSession session) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String login = auth.getName();
-        session.setAttribute("login", login);
-        return "redirect:/";
+        session.setAttribute("username", login);
+        return "redirect:/api/fields/";
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value = {"/edit-profile"}, method = RequestMethod.GET)
     public String viewPeople(Model model, HttpSession session) {
         String username = (String) session.getAttribute("username");
-        User user = userService.findByLogin(username);
+        User user = userService.findByUsername(username);
         model.addAttribute("user", user);
         return "edit-profile";
     }
