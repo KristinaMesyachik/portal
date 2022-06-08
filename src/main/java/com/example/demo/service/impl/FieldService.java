5c173511a1422c6958965ae1931b8dc9e26ab2fb
KristinaMesyachik <mesyachik2001@gmail.com>
2022-06-08 08:52:46 +0300

    Update findAll(pageable)

--- src/main/java/com/example/demo/service/impl/FieldService.java
+++ src/main/java/com/example/demo/service/impl/FieldService.java
@@ -6,13 +6,9 @@ import com.example.demo.repository.IFieldRepository;
 import com.example.demo.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,21 +18,7 @@ public class FieldService implements IService<Field, Long> {
     private IFieldRepository fieldRepository;
 
     public Page<Field> findAll(Pageable pageable) {
-        int pageSize = pageable.getPageSize();
-        int currentPage = pageable.getPageNumber();
-        int startItem = currentPage * pageSize;
-
-        List<Field> collect = fieldRepository.findAll();
-        List<Field> list;
-        if (collect.size() < startItem) {
-            list = Collections.emptyList();
-        } else {
-            int toIndex = Math.min(startItem + pageSize, collect.size());
-            list = collect.subList(startItem, toIndex);
-        }
-        return new PageImpl<>(list, PageRequest.of(currentPage,
-                pageSize),
-                collect.size());
+        return fieldRepository.findAll(pageable);
     }
 
     public Field findById(Long id) {
