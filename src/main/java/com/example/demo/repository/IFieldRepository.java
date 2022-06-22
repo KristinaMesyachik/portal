04edea46aa1dcc41c7378b9dcedec94931ff914f
KristinaMesyachik <mesyachik2001@gmail.com>
2022-06-22 19:06:01 +0300

    Update find method

--- src/main/java/com/example/demo/repository/IFieldRepository.java
+++ src/main/java/com/example/demo/repository/IFieldRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IFieldRepository extends JpaRepository<Field, Long> {
+    List<Field> findByIsActiveTrue();
 }
